745666f461b90152b623aef2bc321c0ff049b910
Aleksandra Gencheva <a.gencheva@student.fontys.nl>
2020-03-27 14:32:47 +0100

    unsaved changes in startup.cs

--- TripAdvisorForEducation.Web/Startup.cs
+++ TripAdvisorForEducation.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TripAdvisorForEducation.Data;
+using TripAdvisorForEducation.Services;
 
 namespace TripAdvisorForEducation.Web
 {
