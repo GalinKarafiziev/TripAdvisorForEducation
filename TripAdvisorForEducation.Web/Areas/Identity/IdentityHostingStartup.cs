a18faa0d7e672ea7ec21337ef54a4d585261534f
Veselin Popov <veselinpopov1999@gmail.com>
2020-04-21 15:28:51 +0200

    scaffolded login/register

---
+++ TripAdvisorForEducation.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TripAdvisorForEducation.Web.Data;
+
+[assembly: HostingStartup(typeof(TripAdvisorForEducation.Web.Areas.Identity.IdentityHostingStartup))]
+namespace TripAdvisorForEducation.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TripAdvisorForEducationWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TripAdvisorForEducationWebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TripAdvisorForEducationWebContext>();
+            });
+        }
+    }
+}
