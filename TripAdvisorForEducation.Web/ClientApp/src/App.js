210098c45ddd23ca5aa28bad02fe5111ad64e306
Redzhep <redjep.molaahmed.rm@gmail.com>
2020-04-23 12:21:47 +0200

    Clean up the project

--- TripAdvisorForEducation.Web/ClientApp/src/App.js
+++ TripAdvisorForEducation.Web/ClientApp/src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
@@ -14,13 +10,11 @@ export default class App extends Component {
   static displayName = App.name;
 
   render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <AuthorizeRoute path='/fetch-data' component={FetchData} />
-        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-      </Layout>
+      return (
+          <div>
+              <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+              <h1>Hello wolrddds</h1>
+          </div>
     );
   }
 }
