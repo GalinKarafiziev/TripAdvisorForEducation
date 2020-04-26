ff4ee83383500225433ee04365a1d0f9c17e721d
Veselin Popov <veselinpopov1999@gmail.com>
2020-04-26 14:55:20 +0200

    changed folder structure

--- TripAdvisorForEducation.Web/ClientApp/src/App.js
+++ TripAdvisorForEducation.Web/ClientApp/src/App.js
@@ -4,17 +4,20 @@ import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
 
-import './custom.css'
+import './custom.css';
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
-      return (
-          <div>
-              <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-              <h1>Hello wolrddds</h1>
-          </div>
+  render() {
+    return (
+      <div>
+        <Route
+          path={ApplicationPaths.ApiAuthorizationPrefix}
+          component={ApiAuthorizationRoutes}
+        />
+        <h1>Hello wolrddds</h1>
+      </div>
     );
   }
 }
